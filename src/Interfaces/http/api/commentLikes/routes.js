3b975e1f9d557e678bae3d83b8bf7f1f28e94477
dwikybp2 <dwikybaguspasetyo@gmail.com>
2023-11-05 09:51:49 +0700

    skenario pass CI

--- src/Interfaces/http/api/commentLikes/routes.js
+++ src/Interfaces/http/api/commentLikes/routes.js
@@ -1,12 +1,12 @@
 const routes = (handler) => ([
-  // {
-  //   method: 'PUT',
-  //   path: '/threads/{threadId}/comments/{commentId}/likes',
-  //   handler: handler.putCommentLikesHandler,
-  //   options: {
-  //     auth: 'forumapi_jwt',
-  //   },
-  // },
+  {
+    method: 'PUT',
+    path: '/threads/{threadId}/comments/{commentId}/likes',
+    handler: handler.putCommentLikesHandler,
+    options: {
+      auth: 'forumapi_jwt',
+    },
+  },
 ]);
 
 module.exports = routes;
